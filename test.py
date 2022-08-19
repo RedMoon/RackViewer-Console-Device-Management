ebdd62266b1ab13cee9799a69bd69e33d6e78872
Alexandr Motyrev <47520961+RedMooner@users.noreply.github.com>
2022-08-19 15:54:56 +0500

    Variables Update 0.1
    added the ability to create variables
    var a = 10 # creates a variable
    $ a # outputs

--- test.py
+++ test.py
@@ -2,17 +2,26 @@ from ast import arg
 from posixpath import split
 import sys  # Импортируем библиотеку sys
 import os  # Импортируем библиотеку os
-
+class variable:
+    variables = {"version": 1.0}
+    
 class Tokens:
-    main = ["sum","delete"]
+    main = ["sum","delete","var","$"]
     def func_factory(operation,args):
         if operation == 'sum':
             def summ():
                 return sum([int(item) for item in args])
             return summ
-        elif operation == 'delete':
-            def mul(a, b):
-                return a * b
+        elif operation == 'var':
+            def mul():
+                if(args[1] == "="):
+                    variable.variables[args[0]] = args[2]
+                    return variable.variables[args[0]]
+                else: return "error = is unexpected"
+            return mul
+        elif operation == '$':
+            def mul():
+               return variable.variables[args[0]]
             return mul
         else:
             raise Exception
@@ -22,7 +31,7 @@ class Lexer:
         if Tokens.main.__contains__(vars[0]):
             func = Tokens.func_factory(vars[0],args=vars[1:])
             print(func())
-print("Welcome to the Rockviewer Console Device Management RVC")
+print("Welcome to the Rackviewer Console Device Management RVC")
 while(True):
     value = input("->")
     Lexer.parse(value.split(" "))
