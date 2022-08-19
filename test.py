dcd4400f715174bb756fff09f777a084541e3700
Alexandr Motyrev <47520961+RedMooner@users.noreply.github.com>
2022-08-19 15:30:39 +0500

    fix of the last update

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class Tokens:
     def func_factory(operation,args):
         if operation == 'sum':
             def summ():
-                return int(args[0]) + int(args[1])
+                return sum([int(item) for item in args])
             return summ
         elif operation == 'delete':
             def mul(a, b):
@@ -20,7 +20,7 @@ class Tokens:
 class Lexer:
     def parse( vars):
         if Tokens.main.__contains__(vars[0]):
-            func = Tokens.func_factory(vars[0],args=[vars[1],vars[2]])
+            func = Tokens.func_factory(vars[0],args=vars[1:])
             print(func())
 print("Welcome to the Rockviewer Console Device Management RVC")
 while(True):
